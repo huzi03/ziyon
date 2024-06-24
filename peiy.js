6cd9369e19b4e46a07954afc5a55d2c63ea21231
huzi03 <xtdzzzzz01@outlook.com>
2024-06-24 13:31:01 +0800

    Update peiy.js

--- peiy.js
+++ peiy.js
@@ -5,7 +5,7 @@ peiy
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.next\.bspapp\.com url script-response-body https://raw.githubusercontent.com/yourusername/QuantumultX/main/orange_voice.js
+^https?:\/\/api\.next\.bspapp\.com url script-response-body https://raw.githubusercontent.com/huzi03/ziyon/main/peiy.js
 
 [mitm]
 hostname = api.next.bspapp.com
