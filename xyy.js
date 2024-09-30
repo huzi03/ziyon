a643675bce5869515c535252e1709933dd059830
huzi03 <xtdzzzzz01@outlook.com>
2024-09-30 21:21:07 +0800

    Create xyy.js

---
+++ xyy.js
@@ -0,0 +1,20 @@
+/*************************************
+[rewrite_local]
+# 匹配 `htx.jietuguanjia.com` 的特定 API 并修改响应内容
+http://htx.jietuguanjia.com/api/app/userInfo url script-response-body xyy.js
+
+[mitm]
+hostname = htx.jietuguanjia.com
+*************************************/
+let body = $response.body;
+
+// 替换 `isInAppBuy` 值为 `0`
+body = body.replace(/isInAppBuy":0/g, 'isInAppBuy":1');
+
+// 替换 `isVip` 值为 `false`
+body = body.replace(/isVip":false/g, 'isVip":true');
+
+// 替换 `vipExpiredTime` 为特定时间
+body = body.replace(/vipExpiredTime":null/g, 'vipSignTime":"2099-09-30 20:52:09"');
+
+$done({ body });
