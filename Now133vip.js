83cac1a707d4b6f19dc84fa8f4a1f8aea3f3cee0
huzi03 <xtdzzzzz01@outlook.com>
2026-03-04 12:01:54 +0800

    Update and rename Now122vip.js to Now133vip.js

--- Now133vip.js
+++ Now133vip.js
@@ -10,7 +10,7 @@
 *******************************
 [rewrite_local]
 # > Now 正念冥想 VIP解锁 + 课程解锁
-^https?://nowapi\.navoinfo\.cn/(h2/(activity/refreshAppConfig|order/getNormalCourseSku)|app_config_info|get_sections_list) url script-response-body https://raw.githubusercontent.com/huzi03/ziyon/refs/heads/main/Nowvip.js
+^https?://nowapi\.navoinfo\.cn/(h2/(activity/refreshAppConfig|order/getNormalCourseSku)|app_config_info|get_sections_list) url script-response-body https://raw.githubusercontent.com/huzi03/ziyon/refs/heads/main/Now133vip.js
 
 [mitm]
 hostname = nowapi.navoinfo.cn, noweocdn.navolove.com
